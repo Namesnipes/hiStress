7de438e9c273a4a6bcc3da2baa238d1582ddc638
MooMan <36575231+Namesnipes@users.noreply.github.com>
2022-03-25 22:00:14 -0700

    Added functionality to the canvas
    proof of concept of a working progress bar (press z)!

---
+++ js/controls.js
@@ -0,0 +1,24 @@
+keys = {
+    90: ['z', 0, false],
+    88: ['x', 1, false],
+    78: ['n', 2, false],
+    77: ['m', 3, false]
+};
+
+
+document.body.onkeydown = function(e) {
+    if (document.activeElement.tagName != "INPUT" && typeof keys[e.keyCode] != 'undefined') {
+        var isDown = keys[e.keyCode][1]
+        if(isDown) return
+        keys[e.keyCode][2] = true
+        keyPress(Number(keys[e.keyCode][1]),false);
+    }
+};
+
+document.body.onkeyup = function(e) {
+    if (document.activeElement.tagName != "INPUT" && typeof keys[e.keyCode] != 'undefined') {
+        var isDown = keys[e.keyCode][1]
+        keys[e.keyCode][2] = false
+        keyPress(Number(keys[e.keyCode][1]),true);
+    }
+};
---
+++ js/script.js
@@ -0,0 +1,70 @@
+var PROGRESS_BAR_HEIGHT = 600
+var PROGRESS_BAR_WIDTH = 30
+var PLAY_BAR_HEIGHT = 600
+var PLAY_BAR_WIDTH = 70
+
+var workbar = document.getElementById("workbar");
+var workbarWidth = workbar.width
+var workbarCanvas = workbar.getContext("2d");
+
+var work = document.getElementById("work");
+var workCanvas = work.getContext("2d");
+
+
+var progressBars = [{"canvas": workbarCanvas}]
+var playBars = [{"canvas": workCanvas, "ypos":0, "movement":1}]
+
+//fills up a progress bar to corresponding percentage
+function setProgressBar(canvasId,percent){ // canvas id, percent out of 100%
+  var canvas = progressBars[canvasId].canvas
+  var percentDecimal = percent/100
+  var yCoord = 600 - (600 * percentDecimal)
+  var height = 600 - yCoord
+
+  canvas.beginPath();
+  canvas.rect(0,yCoord, PROGRESS_BAR_WIDTH, height); //x, y, width, height
+  canvas.fillStyle = "green";
+  canvas.fill();
+}
+
+//moves the bar in a play bar to corresponding y coordinate
+function setPlayBar(canvasId,yCoord){// canvas id, y coordinate of bar (0 is the top, 600 is bottom)
+  var canvas = playBars[canvasId].canvas
+  var barHeight = 170
+
+  if((PLAY_BAR_HEIGHT - yCoord) < barHeight || yCoord < 0) {return} //stop the bar from going through the bottom of the canvas
+
+  canvas.clearRect(0, 0, PLAY_BAR_WIDTH, PLAY_BAR_HEIGHT);
+  canvas.beginPath();
+  canvas.rect(0,yCoord, PLAY_BAR_WIDTH, 170); //x, y, width, height
+  canvas.fillStyle = "green";
+  canvas.fill();
+  playBars[canvasId].ypos = yCoord
+}
+
+
+function barGravity(canvasId){
+  var canvas = playBars[canvasId].canvas
+  var currentYPos = playBars[canvasId].ypos
+  var relativeMovement = playBars[canvasId].movement
+  setPlayBar(canvasId,currentYPos+relativeMovement)
+}
+
+function setbarGravity(sign){
+
+}
+
+function setup(){
+  setInterval(barGravity,1000/60,0)
+}
+
+function keyPress(canvasId,isUp) {
+  console.log(canvasId)
+  playBar = playBars[canvasId].canvas
+  playBars[canvasId].movement = 1 * (isUp && 1 || -1)
+
+}
+
+setup()
+setPlayBar(0,0)
+setProgressBar(0,30)
